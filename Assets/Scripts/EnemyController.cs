35421dd216da43deacbcfe76f8e10e715617e2cf
Balraj Johal <balraj-johal@users.noreply.github.com>
2021-06-07 18:26:10 +0100

    enemy hp and bullet force

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -8,27 +8,51 @@ public class EnemyController : MonoBehaviour
     //NAV Stuff
     [SerializeField] float keepAwayDist = 15f;
     [SerializeField] float speed = 3.5f;
-    NavMeshAgent agent; 
+    // NavMeshAgent agent; 
 
     //ATTACK stuff
     [SerializeField] float attackRate = 3f;
     float nextAttackTime = 0;
 
+    //ENEMY stuff
+    public int hp = 100;
+    Rigidbody rb;
+
     // Start is called before the first frame update
     void Start() {
-        agent = GetComponent<NavMeshAgent>();
-        agent.stoppingDistance = keepAwayDist;
-        agent.speed = speed;
+        rb = GetComponent<Rigidbody>();
+        // agent = GetComponent<NavMeshAgent>();
+        // agent.stoppingDistance = keepAwayDist;
+        // agent.speed = speed;
     }
 
     // Update is called once per frame
     void Update() {
-        agent.destination = PlayerController.instance.transform.position;
+        DeathCheck();
 
+        //move navmesh agent
+        // agent.destination = PlayerController.instance.transform.position;
+
+        //attack timer
         if(Time.time > nextAttackTime) {
             nextAttackTime = Time.time + attackRate;
             // print("ATTACK!");
         }
     }
 
+    //DAMAGE FUNCTIONS
+    public void TakeDamage(int damage) {
+        hp -= damage;
+        Debug.Log("NEW HP: " + hp);
+    }
+    public void DeathCheck() {
+        if (hp <= 0) {
+            Destroy(gameObject);
+        }
+    }
+    public void ApplyForce(Vector3 bulletVelocity) {
+        print("applyingforce");
+        bulletVelocity = bulletVelocity;
+        rb.AddForce(bulletVelocity.x, 0, bulletVelocity.z);
+    }
 }
--- Assets/Scripts/Projectiles/ProjectileMove.cs
+++ Assets/Scripts/Projectiles/ProjectileMove.cs
@@ -6,6 +6,7 @@ public class ProjectileMove : MonoBehaviour
 {
     Rigidbody rb;
     public float shotSpeed = 600.0f;
+    public int damage = 10;
 
     // Start is called before the first frame update
     void Start()
@@ -19,21 +20,26 @@ public class ProjectileMove : MonoBehaviour
         // rb.AddForce(transform.forward * speed);
     }
 
-    void OnCollisionEnter(Collision collision) {
-        if (collision.collider.gameObject.layer != LayerMask.NameToLayer("Player"))
+    void OnTriggerEnter(Collider collider) {
+        if (collider.gameObject.layer != LayerMask.NameToLayer("Player"))
         {
             Debug.Log("NOT PLAYER");
-            HitSomething(collision);
+            HitSomething(collider);
         } else {
             Debug.Log("PLAYER");
         }
     }
 
-    void HitSomething(Collision collision) {
-        ContactPoint contact = collision.contacts[0];
-        Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
-        Vector3 position = contact.point;
+    void HitSomething(Collider collider) {
+        // ContactPoint contact = collision.contacts[0];
+        // Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        // Vector3 position = contact.point;
         // Instantiate(explosionPrefab, position, rotation);
-        Destroy(gameObject);
+        Vector3 bulletVelocity = rb.velocity;
+        if (collider.transform.tag == "Cereal") {
+            collider.gameObject.GetComponent<EnemyController>().ApplyForce(bulletVelocity);
+            collider.gameObject.GetComponent<EnemyController>().TakeDamage(damage);
+        }
+        Destroy(gameObject); //destroy Self
     }
 }
